fb348ca81547a45a5cd0ff9c011d4953ed67e65c
paul <p.ensing66@gmail.com>
2018-12-19 15:04:51 +0100

    v1.1

--- calculator.js
+++ calculator.js
@@ -1,165 +1,130 @@
 total=0;
-subtotal=0;
 subtotalstr="";
 lastoperator="+";
-prevbutton="op";
 
 function totalfunction(op) {
- swop = lastoperator;
- lastoperator = op;
- st = subtotal;
- st = parseInt(subtotalstr);
+ st = parseFloat(subtotalstr);
  
- switch (swop) {
+ switch (lastoperator) {
   case "/":
    count = total / st;
-   return count;
+   break;
   case "*":
    count = total * st;
-   return count;
+   break;
   case "-":
    count = total - st;
-   return count;
+   break;
   case "+":
    count = total + st;
-   return count;
+   break;
   case "=":
    count = total + st;
-   return count;
-}
-}
-
-function totalfunctionoud(op) {
- swop = lastoperator;
- lastoperator = op;d
- t = number(total);
- switch (swop) {
-  case "/":
-   count = total / subtotal;
-   return count;
-  case "*":
-   count = total * subtotal;
-   return count;
-  case "-":
-   count = total - subtotal;
-   return count;
-  case "+":
-   count = total + subtotal;
-   return count;
-  case "=":
-   count = total + subtotal;
-   return count;
-}
+   break;
+ }
+ lastoperator = op;
+ return count;
 }
 
 function toetsuit(t) {
  alert("Toets '"+t+"' is niet actief");
 }
 
+function doop(op) {
+
+
+if (op == "=") {
+	if (subtotalstr == "") { alert("Eerst een getal invoeren!"); } 
+	else {
+    document.getElementById("display").innerHTML = "";
+	total = totalfunction(op);
+    document.getElementById("total").innerHTML = total;
+	total = 0;
+	subtotalstr = "";
+    document.getElementById("subtotal").innerHTML = subtotalstr;
+	lastoperator = op;}
+} else {
+	if (subtotalstr == "") { alert("Eerst een getal invoeren!"); } 
+	else {
+    document.getElementById("display").innerHTML += op;
+	total = totalfunction(op);
+    document.getElementById("total").innerHTML = total;
+    document.getElementById("subtotal").innerHTML = subtotalstr;
+	subtotalstr = "";
+	lastoperator = op;}
+}
+
+
+}
+
 function toets(t) {
 switch (t) {
   case "c":
+	total = 0;
+	subtotalstr = "";
     document.getElementById("display").innerHTML = "";
     document.getElementById("total").innerHTML = "0";
-	total = 0;
-	subtotal = 0;
-	subtotalstr = "";
+    document.getElementById("subtotal").innerHTML = subtotalstr;
     break;
   case 0:
     document.getElementById("display").innerHTML += "0";
-	subtotal += 0;
 	subtotalstr += "0";
     break;
   case 1:
     document.getElementById("display").innerHTML += "1";
-	subtotal += 1;
 	subtotalstr += "1";
     break;
   case 2:
     document.getElementById("display").innerHTML += "2";
-	subtotal += 2;
 	subtotalstr += "2";
     break;
   case 3:
     document.getElementById("display").innerHTML += "3";
-	subtotal += 3;
 	subtotalstr += "3";
     break;
   case 4:
     document.getElementById("display").innerHTML += "4";
-	subtotal += 4;
 	subtotalstr += "4";
     break;
   case 5:
     document.getElementById("display").innerHTML += "5";
-	subtotal += 5;
 	subtotalstr += "5";
     break;
   case 6:
     document.getElementById("display").innerHTML += "6";
-	subtotal += 6;
 	subtotalstr += "6";
     break;
   case 7:
     document.getElementById("display").innerHTML += "7";
-	subtotal += 7;
 	subtotalstr += "7";
     break;
   case 8:
     document.getElementById("display").innerHTML += "8";
-	subtotal += 8;
 	subtotalstr += "8";
     break;
   case 9:
     document.getElementById("display").innerHTML += "9";
-	subtotal += 9;
 	subtotalstr += "9";
+    break;
+  case ".":
+    document.getElementById("display").innerHTML += ".";
+	subtotalstr += ".";
     break;
 
   case "/":
-    document.getElementById("display").innerHTML += "/";
-	total = totalfunction("/");
-    document.getElementById("total").innerHTML = total;
-    document.getElementById("subtotal").innerHTML = subtotalstr;
-	subtotal = 0;
-	subtotalstr = "";
-	lastoperator = "/";
+	doop("/");
     break;
   case "*":
-    document.getElementById("display").innerHTML += "*";
-	total = totalfunction("*");
-    document.getElementById("total").innerHTML = total;
-    document.getElementById("subtotal").innerHTML = subtotalstr;
-	subtotal = 0;
-	subtotalstr = "";
-	lastoperator = "*";
+	doop("*");
     break;
   case "-":
-    document.getElementById("display").innerHTML += "-";
-	total = totalfunction("-");
-    document.getElementById("total").innerHTML = total;
-    document.getElementById("subtotal").innerHTML = subtotalstr;
-	subtotal = 0;
-	subtotalstr = "";
-	lastoperator = "-";
+	doop("-");
     break;
   case "+":
-    document.getElementById("display").innerHTML += "+";
-	total = totalfunction("+");
-    document.getElementById("total").innerHTML = total;
-    document.getElementById("subtotal").innerHTML = subtotalstr;
-	subtotal = 0;
-	subtotalstr = "";
-	lastoperator = "+";
+	doop("+");
     break;
   case "=":
-	total = totalfunction("=");
-    document.getElementById("total").innerHTML = total;
-    document.getElementById("display").innerHTML = total;
-    document.getElementById("subtotal").innerHTML = subtotalstr;
-	subtotal = 0;
-	subtotalstr = "";
-	lastoperator = "+";
+	doop("=");
     break;
 }
 }
